144b4d5c0cbc3b931931023c5968a662c4c738f8
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-13 19:02:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |key, value|
-    if value < 13
+    if value < 13learn s
       puts "Happy Birthday #{key}! You are now #{value} years old!"
     else
       puts "You are too old for this."
