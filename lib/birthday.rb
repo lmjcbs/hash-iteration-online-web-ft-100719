d0e4d0d0351025955659b0fe00fd8f33c3db1236
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-13 19:02:55 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,21 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each { |key, value| puts "Happy Birthday #{key}! You are now #{value} years old!"}
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+    # add your code snippet here!
+  birthday_kids.each do |key, value|
+    if value < 13
+      puts "Happy Birthday #{key}! You are now #{value} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,17 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
+require 'pry'
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = nil
+  passengers.each do |key, value|
+    return passengers[:suite_a] if passengers[:suite_a][0] == "A"
+  end
+  winner
 end
